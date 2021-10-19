bcbc81e3150ac88d95c92c8f3d0a247bae22e11d
Ian Judge <ianjudge1969@gmail.com>
2021-10-19 22:32:47 +0100

    removed unnecessary code and added comment header line

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
+# call setup.py to build the c module
 python3 setup.py build_ext --inplace
 
--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
+# setup.py script to build mandlebrot.c as a python module
 from distutils.core import setup, Extension
-import numpy.distutils.misc_util
 
 setup(
     ext_modules=[Extension("mandlebrot", ["mandlebrot.c"])],
-    include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs(),
 )
