2d3c4c1470af1aec86f151b591f097879fddd23e
Ian Judge <ianjudge1969@gmail.com>
2021-10-27 20:00:53 +0100

    running multiprocessing test multiple times

--- test_c_module.py
+++ test_c_module.py
@@ -20,8 +20,8 @@ class Mandlebrot_c_module:
 
     def mp_generate(self, w, h):
         # multiprocessing
-        cpus = int(cpu_count()/2) # should be 2* num cpu cores, any more gives negligible benefit
-        print(cpus, " CPUs")
+        cpus = int(cpu_count()) # should be 2* num cpu cores, any more gives negligible benefit
+        #print(cpus, " CPUs")
         yslice = (self.Ye-self.Ys)/cpus
         params = []
         for n in range(cpus):
@@ -34,14 +34,16 @@ class Mandlebrot_c_module:
 
 
 if __name__ == '__main__':
-    sz = 640
+    sz = 1280
     print("Testing c module")
     mand = Mandlebrot_c_module()
+    
+    for a in range(100):
+        #now = time.time()
+        #mand.generate(sz, sz)
+        #print("Took ", time.time() - now, " secs")
 
-    now = time.time()
-    mand.generate(sz, sz)
-    print("Took ", time.time() - now, " secs")
+        now = time.time()
+        mand.mp_generate(sz, sz)
+        print("Took ", time.time() - now, " secs")
 
-    now = time.time()
-    mand.mp_generate(sz, sz)
-    print("Took ", time.time() - now, " secs")
