90ecc272e41373cacb91dfabf10804448ae78beb
Ian Judge <ianjudge1969@gmail.com>
2021-10-29 18:59:19 +0100

    refactor and tidy up

--- test_mand_mpfr.c
+++ test_mand_mpfr.c
@@ -1,6 +1,7 @@
 /*
+ * module for calculating the madlebrot set using the mpfr library
  *
- *
+   gcc -g -o mandlebrot_mpfr -lmpfr -lm test_mand_mpfr.c
  */
 
 #include <stdio.h>
@@ -34,8 +35,7 @@ static struct Color sqrt_gradient(unsigned int it, unsigned int maxiter)
 		c.g = (int)floor((( sin(0.45 * m * 85.0) *0.5)+0.5) *255);
 		c.b = (int)floor((( sin(0.25 * m * 85.0) *0.5)+0.5) *255);
 		if (c.r>255 || c.g>255 || c.b>255 || c.r<0 || c.g<0 || c.b<0)
-			printf("col(%5.6f,%5.6f,%5.6f) | ",sin(0.30 * m * 20.0),sin(0.45 * m * 20.0),sin(0.65
- * m * 20.0));
+			printf("col(%5.6f,%5.6f,%5.6f) | ",sin(0.30 * m * 20.0),sin(0.45 * m * 20.0),sin(0.65 * m * 20.0));
 	}
 	else
 	{
@@ -56,7 +56,7 @@ static struct Color sqrt_gradient(unsigned int it, unsigned int maxiter)
  *
  * Return int
  */
-int mandlebrot_set_mpfr(const unsigned int xsize,   /* width of screen/display/window */
+void mandlebrot_set_mpfr(const unsigned int xsize,   /* width of screen/display/window */
                         const unsigned int ysize,   /* height of screen/display/window */
                         const unsigned int maxiter, /* max iterations before escape */
                         const char* Xs_str, /* string repr of mpfr_t for X top left */
@@ -104,12 +104,12 @@ int mandlebrot_set_mpfr(const unsigned int xsize,   /* width of screen/display/w
         {
             iteration = 0;
 
-			/* double x0 = scaled(Dx, xsize, Xs, Xe); */
+            /* double x0 = scaled(Dx, xsize, Xs, Xe); */
             mpfr_sub(a, Xe, Xs, MPFR_RNDD);
             mpfr_mul_d(a, a, ((double)Dx/(double)xsize), MPFR_RNDD);
             mpfr_add(x0, a, Xs, MPFR_RNDD);
 
-			/* double y0 = scaled(Dy, ysize, Ys, Ye); */
+            /* double y0 = scaled(Dy, ysize, Ys, Ye); */
             mpfr_sub(a, Ye, Ys, MPFR_RNDD);
             mpfr_mul_d(a, a, ((double)Dy/(double)ysize), MPFR_RNDD);
             mpfr_add(y0, a, Ys, MPFR_RNDD);
@@ -121,18 +121,18 @@ int mandlebrot_set_mpfr(const unsigned int xsize,   /* width of screen/display/w
             /* while (xsq+ysq <= 4 && iteration < maxiter */
             while ((mpfr_cmp(sum_xsq_ysq, four) <= 0) && (iteration < maxiter))  
             {
-		        mpfr_mul(xsq, x, x, MPFR_RNDD);      /* xsq = x*x; */
-		        mpfr_mul(ysq, y, y, MPFR_RNDD);      /* ysq = y*y; */
-		        mpfr_sub(xtmp, xsq, ysq, MPFR_RNDD); /* xtemp = xsq - ysq + x0; */
+                mpfr_mul(xsq, x, x, MPFR_RNDD);      /* xsq = x*x; */
+                mpfr_mul(ysq, y, y, MPFR_RNDD);      /* ysq = y*y; */
+                mpfr_sub(xtmp, xsq, ysq, MPFR_RNDD); /* xtemp = xsq - ysq + x0; */
                 mpfr_add(xtmp, xtmp, x0, MPFR_RNDD);
-		        mpfr_mul(a, two, x, MPFR_RNDD);      /* y = 2.0*x*y + y0; */
+                mpfr_mul(a, two, x, MPFR_RNDD);      /* y = 2.0*x*y + y0; */
                 mpfr_mul(a, a, y, MPFR_RNDD);
                 mpfr_add(y, a, y0, MPFR_RNDD);
-		        mpfr_swap(x, xtmp);                  /* x = xtemp; */
+                mpfr_swap(x, xtmp);                  /* x = xtemp; */
 
                 /* calcs for while test */
                 mpfr_add(sum_xsq_ysq, xsq, ysq, MPFR_RNDD);
-		        iteration++;
+                iteration++;
             }
             /* create a color value and add to result list */
             rgb = sqrt_gradient(iteration, maxiter);
@@ -150,58 +150,25 @@ int mandlebrot_set_mpfr(const unsigned int xsize,   /* width of screen/display/w
 }
 
 /*
- *
+ * test prog to run the mandlebrot_set_mpfr
  */
 int main()
 {
-    unsigned int xsize, ysize, maxiter;
-    //int *bytearray;
-    char *Xs_str;
-    char *Xe_str;
-    char *Ys_str;
-    char *Ye_str;
+    unsigned int xsize = 640;
+    unsigned int ysize = 640;
+    unsigned int maxiter = 1000;
+	
     mpfr_t Xs, Xe, Ys, Ye;
     mpfr_exp_t exp;
 
+    printf("create array size %d\n", (3 * xsize * ysize));
 
-    /*mpfr_inits2(PRECISION, Xs, Xe, Ys, Ye);*/
-    mpfr_init2(Xs, PRECISION);
-    mpfr_init2(Xe, PRECISION);
-    mpfr_init2(Ys, PRECISION);
-    mpfr_init2(Ye, PRECISION);
-    /* 1.768901153038437, -0.05747430607697883 */
-    mpfr_set_d(Xs, -1.768901153038436, MPFR_RNDD);
-    mpfr_set_d(Xe, -1.768901153038438, MPFR_RNDD);
-    mpfr_set_d(Ys, -0.05747430607697882, MPFR_RNDD);
-    mpfr_set_d(Ye, -0.05747430607697884, MPFR_RNDD);
+    int bytearray[3 * xsize * ysize];
 
-    printf("ndigits %d\n", mpfr_get_str_ndigits(10, PRECISION));
+    mandlebrot_set_mpfr(xsize, ysize, maxiter, "-20", "10", "-15", "15", bytearray);
+    mandlebrot_set_mpfr(xsize, ysize, maxiter, "-020", "010", "-015", "015", bytearray);
+    mandlebrot_set_mpfr(xsize, ysize, maxiter, "-2.0", "1.0", "-1.5", "1.5", bytearray);
 
-    Xs_str = mpfr_get_str(NULL, &exp, 10, 0, Xs, MPFR_RNDD);
-    Xe_str = mpfr_get_str(NULL, &exp, 10, 0, Xe, MPFR_RNDD);
-    Ys_str = mpfr_get_str(NULL, &exp, 10, 0, Ys, MPFR_RNDD);
-    Ye_str = mpfr_get_str(NULL, &exp, 10, 0, Ye, MPFR_RNDD);
-
-    printf("str %s %s %s %s \n", Xs_str, Xe_str, Ys_str, Ye_str);
-
-    xsize = 640;
-    ysize = 640;
-    maxiter = 1000;
-
-    printf("create array size %d\n", (3*xsize*ysize));
-    //bytearray = (int*)calloc((size_t)(3*xsize*ysize), sizeof(int));
-    int bytearray[3*xsize*ysize];
-
-    //printf("bytearray size %ld\n", sizeof(bytearray));
-    /*int bc = 0;
-    bytearray[bc] = 1; bc++;
-    bytearray[bc] = 2; bc++;
-    bytearray[bc] = 3; bc++;
-    bytearray[bc] = 4; bc++;
-    bytearray[bc] = 5; bc++;*/
-    /*for (int z = 0; z<100; z++) { printf("%d %d ",z,bytearray[z]); }
-    printf("");*/
-
-    mandlebrot_set_mpfr(xsize, ysize, maxiter, Xs_str, Xe_str, Ys_str, Ye_str, bytearray);
+    return 0;
 }
 
