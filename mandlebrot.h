dc7a6831397b0bf47d3c62d8d58f02be13c0d418
Ian Judge <ianjudge1969@gmail.com>
2021-10-31 12:55:38 +0000

    removed unnecessary comments

--- mandlebrot.h
+++ mandlebrot.h
@@ -14,21 +14,14 @@ void mandlebrot_bytearray_c(const unsigned int wsize,   /* width of screen/displ
 void mandlebrot_mpfr_c( const unsigned int xsize,   /* width of screen/display/window */
                         const unsigned int ysize,   /* height of screen/display/window */
                         const unsigned int maxiter, /* max iterations before escape */
-                        /*const char* Xs_str, / * string repr of mpfr_t for X top left */
-                        /*const char* Xe_str, / * string repr of mpfr_t for X top right */
-                        /*const char* Ys_str, / * string repr of mpfr_t for Y bottom left */
-                        /*const char* Ye_str, / * string repr of mpfr_t for Y bottom right */
                         int bytearray[] /* reference/pointer to result list of color values*/
                        );
 
-void mpfr_zoom_in( /*const char** Xs_str, / * string repr of mpfr_t for X top left */
-                   /*const char** Xe_str, / * string repr of mpfr_t for X top right */
-                   /*const char** Ys_str, / * string repr of mpfr_t for Y bottom left */
-                   /*const char** Ye_str, / * string repr of mpfr_t for Y bottom right */
-                   const unsigned int pX, /* */
+void mpfr_zoom_in( const unsigned int pX, /* */
                    const unsigned int pY, /* */
                    const unsigned int w, /* */
-                   const unsigned int h  /* */
+                   const unsigned int h, /* */
+                   const unsigned int factor /* */
                   );
 void setup_c();
 
